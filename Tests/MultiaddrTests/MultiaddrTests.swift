08321a1879fd76a9530329d5427dc3c279231461
Dean Eigenmann <dean.eigenmann@icloud.com>
2019-04-07 22:54:17 +0200

    Update MultiaddrTests.swift

--- Tests/MultiaddrTests/MultiaddrTests.swift
+++ Tests/MultiaddrTests/MultiaddrTests.swift
@@ -6,7 +6,7 @@ final class MultiaddrTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Multiaddr().text, "Hello, World!")
+//         XCTAssertEqual(Multiaddr().text, "Hello, World!")
     }
 
     static var allTests = [
